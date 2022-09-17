a22e7ac4ef09af11478767815a31566e3459fde2
OursSA <ourssa01@gmail.com>
2022-09-17 12:06:12 -0400

    Map with single circular marker.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -6,11 +6,23 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    invert: 1,
     accessToken: API_KEY
 });
+
+// Place an example marker in Los Angeles
+//let marker = L.marker([34.0522, -118.2437]).addTo(map)
+
+//Change the Los Angeles marker to a circle
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: 'black',
+    fill: true,
+    fillColor: '#ffffa1'
+}).addTo(map)
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
